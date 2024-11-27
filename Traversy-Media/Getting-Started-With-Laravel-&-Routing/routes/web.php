b3eccf175d01b9c18cbac94f8abfac2918d8de74
Mejbaul Mubin <mejbaulmubin@my.uopeople.edu>
2024-11-28 04:06:34 +0600

    Update

--- Traversy-Media/Getting-Started-With-Laravel-&-Routing/routes/web.php
+++ Traversy-Media/Getting-Started-With-Laravel-&-Routing/routes/web.php
@@ -1,155 +1,17 @@
 <?php
 
-use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
     return view('welcome');
 });
 
-//---------------Routes---------------
-Route::get('/jobs', function () {
-    return 'Available Jobs';
-});
-
-Route::get('/jobs2', function () {
-    return '<h1>Available Jobs</h1>';
-});
-
-Route::post('/submit', function () {
-    return 'Submitted';
-});
-
-Route::match(['get', 'post'], '/user', function () {
-    return 'Mejbaul Mubin';
-});
-
-Route::match(['get', 'post'], '/user/profile', function () {
-    return 'Mejbaul Mubin\'s profile';
-});
-
-Route::any('/user/profile/create', function () {
-    return 'Mejbaul Mubin\'s profile is created';
-});
-
-Route::get('/deshboard', function () {
-    return 'Mejbaul Mubin\'s Deshboard';
-})->name('userdeshboard');
+//---------------Response Helper
 
 Route::get('/test', function () {
-    $url = route('userdeshboard');
-    return "<a href='$url'>click here</a>";
+    return response('Hello World');
 });
 
-Route::get('/api/user', function () {
-    return [
-        'name' => 'Mejbaul Mubin',
-        'email' => 'mejbaulmubin786@gmail.com',
-    ];
+Route::get('/error', function () {
+    return response('Hello World', 404);
 });
-
-Route::get('/posts/{id}', function ($id) {
-    return 'Post ' . $id;
-});
-
-Route::get('/posts/{id}/comments/{commentId}', function ($id, $commentId) {
-    return 'Post ' . $id . ' Comment ' . $commentId;
-});
-
-//Local constraints add with Where close
-Route::post('/posts/{id}', function ($id) {
-    return 'Post ' . $id;
-})->where('id', '[0-9]+');
-// where('id', '[0-9]+') shortcuts  whereNumber('id');
-
-Route::put('/posts/{id}', function ($id) {
-    return 'Post ' . $id;
-})->where('id', '[a-zA-Z]+');
-
-// where('id', '[a-zA-Z]+') shortcuts  whereAlpha('id');
-
-// go to Providers/AppServiceProvider.php to add global constraints.
-
-/*
-public function boot(): void
-{
-Route::pattern('id', '[0-9]+'); // it works with any route that has id;
-}
- */
-
-//-------Route Object & Query Params
-Route::get('/test2', function (Request $request) {
-    return [
-        'method' => $request->method(),
-        'root' => $request->root(),
-        'url' => $request->url(),
-        'rull' => $request->fullUrl(),
-        'ip' => $request->ip(),
-        'method' => $request->method(),
-        'method' => $request->method(),
-        'method' => $request->method(),
-        'method' => $request->method(),
-        'method' => $request->method(),
-        'method' => $request->method(),
-        'method' => $request->method(),
-
-    ];
-});
-
-Route::get('/profile', function (Request $request) {
-    return $request->query('name');
-
-});
-
-// http://127.0.0.1:8000/profile?name=Mejbaul
-
-Route::get('/profile1', function (Request $request) {
-    return $request->only('name');
-});
-
-//http://127.0.0.1:8000/profile2?name=Mejbaul&age=32
-
-Route::get('/profile2', function (Request $request) {
-    return $request->only(['name', 'age']);
-});
-
-//http: //127.0.0.1:8000/profile2?name=Mejbaul&age=32
-//http://127.0.0.1:8000/profile2?name=Mejbaul&age=32&sort=desc
-
-Route::get('/profile3', function (Request $request) {
-    return $request->all();
-});
-
-//http: //127.0.0.1:8000/profile2?name=Mejbaul&age=32
-
-Route::get('/user', function (Request $request) {
-    return $request->has('name'); // return true(1) or False (nothing)
-});
-
-//http: //127.0.0.1:8000/profile2?name=Mejbaul&age=32 return 1
-
-Route::get('/user1', function (Request $request) {
-    return $request->input('name');
-
-});
-
-Route::get('/user2', function (Request $request) {
-    return $request->input('name'); // The difference between input and query is input is work with both in query and form.
-
-});
-
-// http://127.0.0.1:8000/user2?name=Mejbaul
-
-Route::get('/user3', function (Request $request) {
-    return $request->input('name', 'Default Name'); // The difference between input and query is input is work with both in query and form.
-
-});
-
-//http://127.0.0.1:8000/user3
-
-Route::get('/user4', function (Request $request) {
-    return $request->except(['name']);
-});
-//http: //127.0.0.1:8000/profile2?name=Mejbaul&age=32
-
-//$request->session()->get('key') - Get a session value by key
