a3a5d0fe4b620b4f8f9b9eae2238e2be5c623c6c
Mejbaul Mubin <mejbaulmubin@my.uopeople.edu>
2024-11-29 13:40:02 +0600

    Update View & controller

--- Traversy-Media/Getting-Started-With-Laravel-&-Routing/routes/web.php
+++ Traversy-Media/Getting-Started-With-Laravel-&-Routing/routes/web.php
@@ -5,11 +5,3 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
-
-//---------------Views & Controllers
-
-//---------------Create & Display views
-
-Route::get('/jobs', function () {
-    return view('jobs');
-})->name('jobs');
