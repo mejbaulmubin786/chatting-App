a7a5caa1c617eedc4f67c390761b2bccdc3c896b
Mejbaul Mubin <mejbaulmubin@my.uopeople.edu>
2024-12-07 03:14:56 +0600

    Update

--- Traversy-Media/ViewsControllers/resources/views/components/nav-link.blade.php
+++ Traversy-Media/ViewsControllers/resources/views/components/nav-link.blade.php
@@ -1,6 +1,4 @@
-@php
-    echo $attributes;
-@endphp
-<a>
+@props(['active'=>true])
+<a {{ $attributes }} class="text-white hover:underline py-2 {{ $active?'text-yellow-500 font-bold':'' }}">
     {{ $slot }}
 </a>
