c14881734e858edf4ac29f870ae5c0ae196061d8
Mejbaul Mubin <mejbaul_mubin@yahoo.com>
2025-09-09 12:17:03 +0600

    registration images added

--- src/pages/Registration.jsx
+++ src/pages/Registration.jsx
@@ -1,15 +1,18 @@
 import React from 'react'
 import Grid from '@mui/material/Grid';
+import RegistrationImg from '../assets/registration.png';
 
 const Registration = () => {
   return (
     <Grid container spacing={2}>
-  <Grid size={8}>
-    <h1>size=8</h1>
-    <h1>size=8</h1>
-  </Grid>
-  
-</Grid>
+      <Grid size={6}>
+        <h1>size=8</h1>
+      </Grid>
+      <Grid size={6}>
+        <img src={RegistrationImg} />
+      </Grid>
+
+    </Grid>
   )
 }
 
