c6a24cca1a6329b8f254c43046c92e6fa3c66315
Mejbaul Mubin <mejbaul_mubin@yahoo.com>
2025-09-20 23:38:13 +0600

    new ui added for forget password

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,5 @@
 import React from 'react'
+import { CssBaseline } from "@mui/material";
 import {
   createRoutesFromElements,
   createBrowserRouter,
@@ -7,12 +8,14 @@ import {
 } from "react-router-dom";
 import Login from './pages/Login';
 import Registration from './pages/Registration';
+import Login2 from './pages/Login2';
 
 const router = createBrowserRouter(
   createRoutesFromElements(
     <>
       <Route path="/login" element={<Login />}></Route>
       <Route path="/" element={<Registration />}></Route>
+      <Route path="/login2" element={<Login2 />}></Route>
     </>
   )
 );
